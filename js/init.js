86ede5767bd625258d9d5d038207f8a9ba74821e
yullm <yull.michael14@gmail.com>
2017-09-09 05:30:21 -0700

    Data implmentation and major visual changes

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
 var heroes;
 var picks = [];
-var counters = [];
+var comparisons = [];
 var compTotals = [];
+var buffTimerIndex = 0;
+var sortingColumn = 0;
+var advantageTip = "Advantage the current picks have over the hero.";
+var winRateTip = "Average win rate of the current picks against the hero.";
+var matchesTip = "Total number of match comparisons against the hero.";
 $(document).ready(function(){
     $("#preview-selector").hide();
     $.ajax({
@@ -16,7 +21,6 @@ $(document).ready(function(){
             var int = document.getElementById("int-portraits");
             $(wrapper).hide();
             for(var i = 0; i < obj.length; i++){
-                console.log(obj[i].name);
                 var wrapper; 
                 obj[i].urlLargePortrait = obj[i].urlLargePortrait.replace("}","");
                 if(obj[i].primaryAttribute === "agility")
@@ -98,7 +102,7 @@ $(document).ready(function(){
                         "background-image":"none"
                     });
                     picks.splice(pos,1);
-                    counters.splice(counters.indexOf(comps),1);
+                    comparisons.splice(comparisons.indexOf(comps),1);
                     drawIcons();
                     calculateCounters();
                     $("#"+obj.name).removeClass("selected");
@@ -117,42 +121,76 @@ $(document).ready(function(){
             e.preventDefault();
         }
    });
-   
+   $("#menu-prompt-but").on("click",function(){
+       $("#menu").show();
+       $("#menu").animate({
+           right: 0+"px"
+       },300);
+   });
+   $("#floating-menu-but").on("click",function(){
+       $("#menu").show();
+       $("#menu").animate({
+           right: 0+"px"
+       },300);
+   });
+   $("#menu-close-but").on("click",function(){
+       $("#menu").animate({
+           right: $("#menu").width()*-1 + "px"
+       },300,function(){
+           $("#menu").hide();
+       });
+   });
+   initLogoBar();
 });
 
 function calculateCounters(el,comps){
     if(el){
         $(el).data("comps",comps);
-        counters.push(comps);
+        comparisons.push(comps);
     }
+    //comparisons holds an entry for each picks comparisons against all other heros
+    //compTotals has one entry per hero and we add each data set from comparisons to find a total / average
     compTotals = [];
     var container = $("#suggestion-container");
     $(container).empty();
-        if(counters[0]){
-        for(var i = 0; i < counters[0].length;i++){
+        if(comparisons[0]){
+            //for every hero comparison in our first comparison object
+        for(var i = 0; i < comparisons[0].length;i++){
+            //add the data from the first comparison for each hero
             compTotals.push({
-                heroName : counters[0][i].heroName,
-                advantage : counters[0][i].advantage,
-                winRate : counters[0][i].winRate,
-                matches : counters[0][i].matches
+                heroName : comparisons[0][i].heroName,
+                advantage : comparisons[0][i].advantage,
+                winRate : comparisons[0][i].winRate,
+                matches : comparisons[0][i].matches
             });
         }
-        for(var i = 0; i < counters.length; i++){
-            for(var j = 0; j < counters[i].length;j++){
-                compTotals[j].advantage += counters[i][j].advantage;
-                //compTotals[j].advantage = parseFloat(compTotals[j].advantage).toFixed(2);
-                compTotals[j].winRate += counters[i][j].winRate;
-                //compTotals[j].winRate = parseFloat(compTotals[j].winRate).toFixed(2);
-                compTotals[j].matches += counters[i][j].matches;
-                //compTotals[j].matches = parseInt(compTotals[j].matches);
+        //for every other comparison set add the hero's values together of every hero
+        for(var i = 1; i < comparisons.length; i++){
+            for(var j = 0; j < comparisons[i].length;j++){
+                compTotals[j].advantage += comparisons[i][j].advantage;
+                compTotals[j].winRate += comparisons[i][j].winRate;
+                compTotals[j].matches += comparisons[i][j].matches;
             }
         }
-        compTotals.sort(function(a,b){
-            return a.advantage - b.advantage;
+        $.each(compTotals,function(index,heroComp){
+            heroComp.advantage = heroComp.advantage/picks.length;
+            heroComp.winRate = heroComp.winRate/picks.length;
         });
-        compTotals.reverse();
+        compTotals.sort(function(a,b){
+            switch(sortingColumn){
+                case 0:
+                    return a.advantage - b.advantage;
+                break;
+                case 1:
+                    return a.winRate - b.winRate;
+                    break;
+                case 2:
+                    return b.matches - a.matches;
+                    break;
+            }
+        });
         $(container).empty();
-        for(var i = 0; i < 10; i++){
+        for(var i = 0; i < 15; i++){
             var valid = true;
             $.each(picks,function(index,obj){
                 if(compTotals[i].heroName === obj.name.toLowerCase())
@@ -170,7 +208,78 @@ function calculateCounters(el,comps){
                 $(itemIcon).css({
                     "background-image":"url("+iconImg+")"
                 });
+                var iconHover = $("<div />",{class:"suggestion-icon-hover"});
+                
+                $(iconHover).html(compTotals[i].advantage.toFixed(2)+"%");
+                $(itemIcon).append(iconHover);
                 $(item).append(itemIcon);
+                var stats = $("<div />",{class:"suggestion-stats"});
+                for(var j = 0; j < 3; j++){
+                    var stat = $("<div />",{class:"stat"});
+                    var ranking = $("<div />",{class:"rank-bar"});
+                    var title = $("<div />",{class:"stat-title"});
+                    var val = $("<div />",{class:"stat-value"});
+                    var rankWidth = 0;
+                    var tooltip = $("#tooltip");
+                    $(title).hover(function(){
+                        var offset = $(this).offset();
+                        var text = $(this).html();
+                        $(this).data("timer",setTimeout(function(){
+                            $(tooltip).show();
+                            $(tooltip).css({
+                                "top": offset.top + 25 + "px",
+                                "left": offset.left - 100 + "px"
+                            });
+                            switch(text){
+                                case "Advantage:":
+                                    $(tooltip).html(advantageTip);
+                                    break;
+                                case "Win Rate:":
+                                    $(tooltip).html(winRateTip);
+                                    break;
+                                case "Matches:":
+                                    $(tooltip).html(matchesTip);
+                                    break;
+                            }
+                        },1000));
+                    },function(){
+                        clearTimeout($(this).data("timer"));
+                        $(tooltip).hide();
+                    });
+                    switch(j){
+                        case 0:
+                            $(title).html("Advantage:");
+                            $(val).html(compTotals[i].advantage.toFixed(2)+"%");
+                            rankWidth = compTotals[i].advantage.toFixed(2)/7 * -100;
+                            break;
+                        case 1:
+                            $(title).html("Win Rate:");
+                            $(val).html(compTotals[i].winRate.toFixed(2)+"%");
+                            rankWidth = compTotals[i].winRate.toFixed(2);
+                            break;
+                        case 2:
+                            $(title).html("Matches:");
+                            $(val).html(compTotals[i].matches);
+                            rankWidth = compTotals[i].matches/1000000 * 100;
+                            break;
+                    }
+                    if(rankWidth > 100){
+                        rankWidth = 100;
+                    }
+                    $(ranking).css({
+                        "width": rankWidth + "%"
+                    });
+                    if(j !== 0){
+                        $(stat).css({
+                            "border-top":"1px solid #888"
+                        });
+                    }
+                    $(stat).append(ranking);
+                    $(stat).append(title);
+                    $(stat).append(val);
+                    $(stats).append(stat);
+                }
+                $(item).append(stats);
                 $(container).append(item);
             }
         }
@@ -223,5 +332,32 @@ function resize(){
     });
 }
 
+function initLogoBar(){
+    createLogoRotation($("#buff-bar"),buffTimerIndex);
+}
+
+function createLogoRotation(container,index){
+    $.each($(container).children(),function(index, val){
+        $(val).hide();
+    });
+    $(container).children().eq(0).show();
+    $(container).children().eq(0).css({opacity: 1});
+    var buffTimer = setInterval(function(){
+        var curr = $(container).children().eq(index++);
+        if(index === $(container).children().length){
+            index = 0;
+        }
+        var next = $(container).children().eq(index);
+        $(curr).animate({
+            opacity: 0
+        },1000,function(){
+            $(curr).hide();
+            $(next).show();
+            $(next).animate({
+                opacity: 1
+            },1000);
+        });
+    }, 4000);
+}
 
 
