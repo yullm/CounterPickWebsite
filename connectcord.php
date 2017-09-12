61820379dc1543753307490150d1e0fac8841a07
yullm <yull.michael14@gmail.com>
2017-09-12 00:17:17 -0700

    Additional php script for testing out cordova

---
+++ connectcord.php
@@ -0,0 +1,38 @@
+<?php
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
+if(!($sock = socket_create(AF_INET, SOCK_STREAM, 0)))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Couldn't create socket: [$errorcode] $errormsg \n");
+}
+
+if(!socket_connect($sock, 'callback.meglobot.com',1447)){
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Couldn't create connect: [$errorcode] $errormsg \n");
+}
+
+$opMessage = $_POST['opMessage']."\r\n";
+
+if( !socket_write ( $sock , $opMessage))
+{
+    $errorcode = socket_last_error();
+    $errormsg = socket_strerror($errorcode);
+     
+    die("Could not send data: [$errorcode] $errormsg \n");
+}
+
+$message = "";
+while(($buf = socket_read($sock,1024,PHP_NORMAL_READ)) !== false ){ 
+   $message .= $buf;
+}
+
+echo $message;
+
+socket_close($sock);
